614026900242be67e51f104aa8d6090205e05114
MohamedElrodainy <mohamedelrodainy1@gmail.com>
2020-07-08 08:21:31 +0200

    Edited The Config

---
+++ js/comments.js
@@ -0,0 +1,17 @@
+/*
+    // the git language is called [markdown] ==> '.md' and it has it's own rules of writing 
+
+    // to clone a repo ==> copy the repo link and go to the cmder ==> git clone clone-link.git
+
+    --- Add Reset and Commit: 
+
+        [1] git status filename==> checks if there are untracked files [they are not in the staging area] in the project
+        [2] git add filename==> adds the project from the working directory to the staging area
+        [3] git commit -m "message"==> delivers the project from the staging area to the local repo with the description message
+        [4] git push filename==> delivers the project from the local repo to the remote rrepo [git hub]
+        [5] git fetch filename==> delivers the project back from the remote repo [github] to the local repo
+        [6] git checkout filename==> delivers the project from the remote repo to the working directory
+        [7] git merge filename==> delivers the project from the remote repo to the working directory
+        [8] git reset head filename==> this unstages the file again from the staging area 
+        [9] git add * ==> this will add all the project files to the staging area
+*/
